0074a22f1353a61da193003825ab1a7ddf246ca3
Prakhar Singh <prakhars76@gmail.com>
2022-07-17 14:14:40 +0530

    links updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -45,7 +45,7 @@ const Projects = () => {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://github.com/Prakhar77s/my-Portfolio-react">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
